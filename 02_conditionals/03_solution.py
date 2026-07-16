dc54920de3f9607ac7555f47f0bbb3ccf99e8522
hi-kuldeep <kumawatkuldeep24@gmail.com>
2026-07-16 12:06:25 +0530

    feat: add validation check for marks over 100 and document grading scale in solution.py

--- 02_conditionals/03_solution.py
+++ 02_conditionals/03_solution.py
@@ -1,9 +1,15 @@
 marks  = input("Please enter your marks: ")
 marks = int(marks)
 
+# Not allowed to enter more than 100
 if marks > 100:
     print(" Marks can't be greater than 100")
     exit()
+
+# 60-  69   D
+# 70-  79   C
+# 80-  89   B
+# 90- 100   A
 
 if marks < 60:
     print("F")
