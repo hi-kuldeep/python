05ae5a1e0eb33514c3df1e0426accd898b557709
hi-kuldeep <kumawatkuldeep24@gmail.com>
2026-07-20 18:43:29 +0530

    fix: update debug decorator argument handling and refine greeting function parameters

--- 08_decorators/02_solution.py
+++ 08_decorators/02_solution.py
@@ -3,8 +3,11 @@
 def debug(func):
     def wrapper(*args , **kwargs):
         func_name = func.__name__
+        # handling position arguments
         args_value = ', '.join(str(arg) for arg in args)
-        kwargs_value = ', '.join( f"{k}:{v}" for k, v in kwargs)
+        # handling keyword arguments
+        kwargs_value = ', '.join( f"{k}:{v}" for k, v in kwargs.items())
+        # check if no argument are passed
         no_args = len(args) == 0 and len(kwargs) == 0
         if not no_args:
             print(f"Calling {func_name}({args_value} , {kwargs_value})")
@@ -17,9 +20,10 @@ def debug(func):
 @debug
 def hello():
     print("Hello World!")
+    
 @debug
-def greeting(name , message):
-    print(f"{message} , {name}")
+def greeting(name , message="Namaste"):
+    print(f" {name}")
 
 hello()
-greeting("kuldeep" , "Good Morning")
+greeting("kuldeep" , message="Good Morning")
