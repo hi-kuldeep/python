0c3a1881f01e935edd3cb3bf5ca660fbcb54780c
hi-kuldeep <kumawatkuldeep24@gmail.com>
2026-07-21 11:18:14 +0530

    feat: implement a cache decorator to memoize function results based on arguments

---
+++ 08_decorators/03_solution.py
@@ -0,0 +1,24 @@
+import time
+
+def cache(func):
+    cache_value = {}
+    
+    def wrapper(*args, **kwargs):
+        if args in cache_value:
+            print(f"Fetching from cache for args: {args}")
+            return cache_value[args]
+        
+        result = func(*args, **kwargs)
+        cache_value[args] = result
+        return result
+        
+    return wrapper
+
+@cache
+def long_running_function(a, b):
+    time.sleep(2)
+    return a + b
+
+print(long_running_function(2, 3))
+print(long_running_function(2, 3))
+print(long_running_function(4, 5))
